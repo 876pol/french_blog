1e6871c8f6df3813fc11747c44a6b2e373e5d314
876pol <paulchenrox3@gmail.com>
2022-04-10 14:11:48 -0400

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import Layout from '../components/layout';
+import Head from 'next/head'
+
 import '../styles/globals.css';
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <title>No et Moi Blog</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
 export default MyApp;
