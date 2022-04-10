52b3e8a7f06caa7f3b53a79bcde201e4b9aa4a45
876pol <paulchenrox3@gmail.com>
2022-04-10 13:04:50 -0400

    update layout.js

--- components/layout.js
+++ components/layout.js
@@ -3,14 +3,14 @@ export default function Layout({ children }) {
     return (
         <div className='flex flex-col min-h-screen'>
             <header className='bg-pink-600 mb-8 py-4'>
-                <div className='container mx-auto flex justify-center text-white text-lg font-medium hover:text-blue-100'>
-                    <span className='mx-auto'><Link href='/'>Réflexions sur &quot;No et moi&quot;</Link></span>{' '}
+                <div className='container mx-auto flex justify-center text-white text-lg font-medium hover:text-gray-300'>
+                    <Link href='/'>Réflexions sur &quot;No et moi&quot;</Link>
                 </div>
             </header>
             <main className='container mx-auto flex-1'>{children}</main>
             <footer className='bg-pink-600 mt-8 py-4'>
                 <div className='container mx-auto flex justify-center text-white text-lg font-medium'>
-                    Paul Chen | <a href='https://github.com/876pol/french_blog'> Github</a>
+                    Paul Chen |&nbsp;<span className='hover:text-gray-300'><Link href='https://github.com/876pol/french_blog'>Github</Link></span>
                 </div>
             </footer>
         </div>
